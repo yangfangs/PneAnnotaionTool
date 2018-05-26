bd1ffd50392800a0cfbf382c76f9e4cffb5c684c
yangfang <506528950@qq.com>
2018-05-26 19:52:36 +0800

    add Dependence

--- setup.py
+++ setup.py
@@ -10,9 +10,15 @@ setup(
     entry_points={
         "console_scripts": ['PneAnnotaionTool = main:main']
     },
+    install_requires=[
+        "numpy",
+        "pydicom",
+        "matplotlib",
+        "pandas",
+    ],
     url='https://github.com/yangfangs/PneAnnotaionTool',
     license='GNU General Public License v3.0',
-    author='yangfang',
+    author='yang　fang',
     author_email='yangfangscu@gmail.com',
     description='A too for DICOM format figure annotation'
 )
